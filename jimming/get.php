abd55d1dfc802bb4cf25417499d9d05ddc66d2a9
hwang_jimming <N1046710@ntub.edu.tw>
2015-11-15 08:33:15 +0000

    mytest

---
+++ jimming/get.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">   
+ </head>   
+<body>
+<?php
+$MyName=$_POST['MyName'];
+$MyNamee=$_POST['MyNamee'];
+echo $MyName;
+echo $MyName;
+?>
+</body>
+    
+</html>
+
---
+++ school_test/JSfile/main.js
@@ -0,0 +1,10 @@
+function init(){
+    alert('hallo word!');
+}
+
+//宣告一變數outter1
+var outter1 = document.getElementById("outter1");
+//宣告一變數inner1
+var inner1 = document.getElementById("inner1");
+
+outter1.addEventListener
